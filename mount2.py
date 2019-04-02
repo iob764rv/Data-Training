ce9dfd4de3ea1a1deed453fed6bca1a58af71b0c
iob764rv <iic287m@gmail.com>
2019-04-02 19:47:39 -0400

    Update mount2.py

--- mount2.py
+++ mount2.py
@@ -13,3 +13,9 @@ w = np.linspace(-1.5,1.5,100)
 def grad_opt_adapt(eval, winit, nit=1000, lr_init=1e-3):
     f0, fgrad0 = eval(s0)
     lr = lr_init
+    hist = {'lr': [], 'w': [], 'f': []}
+
+    for it in range(nit):
+
+        # Take a gradient step
+        a1 = a0 - lr*fgrad0
