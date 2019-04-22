8f8f22f749b51f29d046f27b463e2ee530c71b5c
iob764rv <iic287m@gmail.com>
2019-04-22 18:38:40 -0400

    Update mount2.py

--- mount2.py
+++ mount2.py
@@ -9,6 +9,8 @@ df.head()
 #xname=
 a = np.array([1,0,-2,0.5,0])
 w = np.linspace(-1.5,1.5,100)
+ystr= df['gene'].values
+
 
 def grad_opt_adapt(eval, winit, nit=1000, lr_init=1e-3):
     f0, fgrad0 = eval(s0)
@@ -19,3 +21,4 @@ def grad_opt_adapt(eval, winit, nit=1000, lr_init=1e-3):
 
         # Take a gradient step
         a1 = a0 - lr*fgrad0
+     
