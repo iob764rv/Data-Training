59a7a4e764566fc1ffd0b6ecfd3e5d66c0571b42
iob764rv <iic287m@gmail.com>
2019-07-15 12:42:00 -0400

    Update mount4.py
    import

--- mount4.py
+++ mount4.py
@@ -4,3 +4,8 @@
  import pickle
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import r2_scor
+
+ import requests 
+ from tqdm import tqdm, tqdm_notebook 
+ from urllib.request import urlopen 
+ import os
