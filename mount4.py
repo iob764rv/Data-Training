3e9b9ba888318e5281a65a40f657805f866dd355
iob764rv <iic287m@gmail.com>
2019-08-01 22:24:02 -0400

    Update mount4.py
    duplicate imports

--- mount4.py
+++ mount4.py
@@ -3,8 +3,6 @@
  import numpy as np 
  import matplotlib.pyplot as plt 
  import pickle
- import requests
- import os
 from sklearn.linear_model import LinearRegression 
 from sklearn.metrics import r2_scor
 
