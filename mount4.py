bb53633fa9604d53f947a2af40e037b9ea0d376a
iob764rv <iic287m@gmail.com>
2019-08-21 18:45:52 -0400

    Update mount4.py

--- mount4.py
+++ mount4.py
@@ -42,4 +42,5 @@ print(rsq)
 #for i in range(nout):
 #plt.subplot(1,nout,i+1)
 #plt.plot(yts[:,i],yhat[:,i],'o') 
-#plt.grid() plt.xlabel('True') plt.ylabel('Predicted')
+#plt.grid() plt.xlabel('True') 
+#plt.ylabel('Predicted')
