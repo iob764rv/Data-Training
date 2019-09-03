bda3947d24c5989d56fbd067918943636211dc39
iob764rv <iic287m@gmail.com>
2019-09-03 18:47:06 -0400

    Update mount4.py

--- mount4.py
+++ mount4.py
@@ -47,5 +47,5 @@ for i in range(nout):
  
  ##= \sum_{j=0}^{p-1} X[i,j]*w[j,k] + b[k]
  n,p = X.shape 
- #Xdly = np.zeros((n-dly,(dly+1)*p)) 
+ Xdly = np.zeros((n-dly,(dly+1)*p)) 
  #for i in range(dly+1):
