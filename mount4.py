53362b43e95a77e52dd9d32a65dab29251ae0fbc
iob764rv <iic287m@gmail.com>
2019-09-10 18:53:40 -0400

    Update mount4.py

--- mount4.py
+++ mount4.py
@@ -80,5 +80,5 @@ dmax = 15
 # Xdly, ydly = ... Xdly, ydly = create_dly_data(Xred,yred,dmax)
 
 for it, d in enumerate(dtest): 
-# TODO: # Xdly1 = Xdly with the columns corresponding to only the `d+1` most recent times. Xdly1 = Xdly[:,:(d+1)*nneuron]
+# Xdly1 = Xdly with the columns corresponding to only the `d+1` most recent times. Xdly1 = Xdly[:,:(d+1)*nneuron]
 # TODO # Split the data (Xdly1,ydly) into training and test # Xtr = ... # ytr = ... # Xts = ... # yts = ... Xtr = Xdly1[Itr,:] ytr = ydly[Itr] Xts = Xdly1[Its,:] yts = ydly[Its]
