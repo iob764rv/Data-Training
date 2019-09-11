3acf1fe9e4681633d5f6675b89d6972b45a3fdc4
iob764rv <iic287m@gmail.com>
2019-09-11 09:26:57 -0400

    Update mount4.py

--- mount4.py
+++ mount4.py
@@ -83,4 +83,5 @@ for it, d in enumerate(dtest):
 # Xdly1 = Xdly with the `d+1` most recent times. 
  #Xdly1 = Xdly[:,:(d+1)*nneuron]
 # TODO # Split the data (Xdly1,ydly) into training and test # Xtr = ...
-# ytr = ... # Xts = ... # yts = ... Xtr = Xdly1[Itr,:] ytr = ydly[Itr] Xts = Xdly1[Its,:] yts = ydly[Its]
+# ytr = ... # Xts = ... # yts = ... 
+#Xtr = Xdly1[Itr,:] ytr = ydly[Itr] Xts = Xdly1[Its,:] yts = ydly[Its]
