876b0b5af079f71a10f40bf67249d97f60e7ac5b
iob764rv <iic287m@gmail.com>
2019-11-21 01:25:03 -0500

    Update mount0.py

--- mount0.py
+++ mount0.py
@@ -40,3 +40,6 @@ plt.ylabel('Current I2 (A)')
 RSS_test = np.mean((ytest-ytest_pred)**2) / np.mean((ytest-np.mean(ytest))**2) 
 
 RSS_test 
+
+model = LinearRegression() model.fit(X,y) # Fits a linear model for a data matrix 
+
