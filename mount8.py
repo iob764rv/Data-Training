fcc901346dc980b20637391fdee9d7f0e3e0906f
iob764rv <iic287m@gmail.com>
2020-03-17 15:17:06 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -101,4 +101,6 @@ tn.ncon([ones, ones, ones, ones],
 ones = np.ones((2, 2))
 tn.ncon([ones, ones], [[-1, 1], [1, -2]])
 
-#diagonal_array =
+#diagonal_array =np.array([[2.0, 0.0, 0.0],
+                           [0.0, 2.5, 0.0],
+                           [0.0, 0.0, 1.5]]) 
