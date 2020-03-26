4cc94001b93cd9dac531061f332a844bc81eabe0
iob764rv <iic287m@gmail.com>
2020-03-25 23:11:43 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -122,7 +122,7 @@ u, vh, truncation_error = tn.split_node(
     a, left_edges=[a[0]], right_edges=[a[1]], max_singular_values=2)
 #a = tn.Node(diagonal_array)
 #u, vh, truncation_error = tn.split_node(
-#    a, left_edges=[a[0]], right_edges=[a[1]], max_singular_values=2)
+
 
 print(truncation_error)
 a = np.ones((4096, 4096))
