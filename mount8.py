71ca2e3cd8e016f642f6796ab80c22c6435ec931
iob764rv <iic287m@gmail.com>
2020-03-27 22:27:27 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -138,3 +138,6 @@ tn.set_default_backend("tensorflow")
 
 class TNLayer(tf.keras.layers.Layer):
  #  super(TNLayer, self).__init__()
+    self.a_var = tf.Variable(tf.random.normal(
+            shape=(8, 8, 2), stddev=1.0/16.0),
+             name="a", trainable=True)
