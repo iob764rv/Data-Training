dce073da892d18069fa1a62e901e7dcbe55a28b5
iob764rv <iic287m@gmail.com>
2020-03-31 16:36:45 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -158,3 +158,6 @@ class TNLayer(tf.keras.layers.Layer):
       c = a @ x_node
       result = (c @ b).tensor
 #      return result + bias_var
+result = tf.vectorized_map(
+        lambda vec: f(vec, self.a_var, self.b_var, self.bias), inputs)
+    return tf.nn.swish(tf.reshape(result, (-1, 64)))
