8f60ca1472a6dca22c59f8bd9d77d9783386dee6
iob764rv <iic287m@gmail.com>
2020-03-30 23:25:51 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -148,3 +148,5 @@ class TNLayer(tf.keras.layers.Layer):
   def call(self, inputs):
     def f(input_vec, a_var, b_var, bias_var):
       input_vec = tf.reshape(input_vec, (8,8))
+      # create the network.
+      a = tn.Node(a_var)
