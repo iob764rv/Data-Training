3f33662962399f9b9c1402f853f4130be9adce7c
iob764rv <iic287m@gmail.com>
2020-04-01 14:06:07 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -167,3 +167,7 @@ Dense = tf.keras.layers.Dense
 #fc_model = tf.keras.Sequential(
  #   [
   #   tf.keras.Input(shape=(2,)),
+#Dense(64, activation=tf.nn.swish),
+ #    Dense(64, activation=tf.nn.swish),
+  #   Dense(1, activation=None)])
+#fc_model.summary()
