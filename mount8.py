1d7fa7030406cfc66f3ee86aa3a2d9dd71bcb034
iob764rv <iic287m@gmail.com>
2020-04-03 19:34:11 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -176,7 +176,7 @@ fc_model.summary()
 tn_model = tf.keras.Sequential(
     [
      tf.keras.Input(shape=(2,)),
-    # Dense(64, activation=tf.nn.swish),
+     Dense(64, activation=tf.nn.swish),
      # Here, we replace the dense layer with our MPS.
     # TNLayer(),
      Dense(1, activation=None)])
