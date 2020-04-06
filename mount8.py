01c93a125ee78d196b1193ff99b77de54d105941
iob764rv <iic287m@gmail.com>
2020-04-06 18:59:27 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -195,3 +195,10 @@ x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
 y_min, y_max = X[:, 1].min() - 5, X[:, 1].max() + 5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
+
+Z = tn_model.predict(np.c_[xx.ravel(), yy.ravel()]) 
+
+
+#Z = Z.reshape(xx.shape)
+#plt.contourf(xx, yy, Z)
+#plt.axis('off')
