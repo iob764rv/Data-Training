6eeaf83f6b64338533af7420f1def99e9ef13527
iob764rv <iic287m@gmail.com>
2020-04-08 17:43:06 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -215,4 +215,5 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
 
 Z = tn_model.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
-plt.contourf(xx, yy, Z)
+
+#plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
