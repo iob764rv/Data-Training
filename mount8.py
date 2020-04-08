de254559fd86f0f1724d31d9f6f08b3db581b749
iob764rv <iic287m@gmail.com>
2020-04-08 17:41:02 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -211,4 +211,4 @@ x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
 y_min, y_max = X[:, 1].min() - 5, X[:, 1].max() + 5
 
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
- #                    np.arange(y_min, y_max, h))
+                     np.arange(y_min, y_max, h))
