5e8578dd26d1936d4b3f8bcc618102a46b6e9362
iob764rv <iic287m@gmail.com>
2020-04-08 17:43:58 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -217,3 +217,11 @@ Z = tn_model.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
 #plt.scatter(X[:, 0], X[:, 1], c=Y, cmap=plt.cm.Paired)
+#fc_model.compile(optimizer="adam", loss="mean_squared_error")
+#fc_model.fit(X, Y, epochs=300, verbose=0)
+
+#h = 1.0
+#x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
+#y_min, y_max = X[:, 1].min() - 5, X[:, 1].max() + 5
+#xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+#                     np.arange(y_min, y_max, h))
