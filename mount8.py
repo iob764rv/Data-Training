e7a0fb3730997689fe486c66460a9bdb84e5f26e
iob764rv <iic287m@gmail.com>
2020-04-15 00:21:33 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -221,7 +221,7 @@ fc_model.compile(optimizer="adam", loss="mean_squared_error")
 fc_model.fit(X, Y, epochs=300, verbose=0)
 
 h = 1.0
-#x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
+x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
 #y_min, y_max = X[:, 1].min() - 5, X[:, 1].max() + 5
 #xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
 #                     np.arange(y_min, y_max, h))
