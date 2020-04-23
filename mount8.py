1fa208ee8b3ef0c800289b0257f69841a69faf13
iob764rv <iic287m@gmail.com>
2020-04-22 23:58:22 -0400

    Update mount8.py

--- mount8.py
+++ mount8.py
@@ -246,7 +246,6 @@ class TNLayer(tf.keras.layers.Layer):
 
   def __init__(self):
     super(TNLayer, self).__init__()
-    # Create the variables for the layer.
     self.a_var = tf.Variable(tf.random.normal(
             shape=(8, 8, 2), stddev=1.0/16.0),
              name="a", trainable=True)
