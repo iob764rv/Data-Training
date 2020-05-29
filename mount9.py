67ff6741ba2af462b567c3fe66578efda7bfadd0
iob764rv <iic287m@gmail.com>
2020-05-28 23:04:48 -0400

    Update mount9.py

--- mount9.py
+++ mount9.py
@@ -5,3 +5,13 @@ import tensorflow as tf
 tf.enable_v2_behavior()
 import tensornetwork as tn
 tn.set_default_backend("tensorflo
+
+                      
+  def __init__(self):
+    super(TNLayer, self).__init__()
+    self.a_var = tf.Variable(tf.random.normal(
+            shape=(8, 8, 2), stddev=1.0/16.0),
+             name="a", trainable=True)
+    self.b_var = tf.Variable(tf.random.normal(shape=(8, 8, 2), stddev=1.0/16.0),
+                             name="b", trainable=True)
+    self.bias = tf.Variable(tf.zeros(shape=(8, 8)), name="bias", trainable=True)
