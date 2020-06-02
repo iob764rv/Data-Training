7a4507145589ea81b18cebb40a63b78b55c83dda
iob764rv <iic287m@gmail.com>
2020-06-02 10:28:46 -0400

    Update mount9.py

--- mount9.py
+++ mount9.py
@@ -33,6 +33,7 @@ tn.set_default_backend("tensorflo
       result = (c @ b).tensor
       return result + bias_var
                        
+     
       result = tf.vectorized_map(
         lambda vec: f(vec, self.a_var, self.b_var, self.bias), inputs)
    return tf.nn.swish(tf.reshape(result, (-1, 64)))
