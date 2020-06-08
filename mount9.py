c0720671197d3e1b3de4add2538ea419760c712a
iob764rv <iic287m@gmail.com>
2020-06-08 19:28:43 -0400

    Update mount9.py

--- mount9.py
+++ mount9.py
@@ -73,3 +73,9 @@ x_min, x_max = X[:, 0].min() - 5, X[:, 0].max() + 5
 y_min, y_max = X[:, 1].min() - 5, X[:, 1].max() + 5
 xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
                      np.arange(y_min, y_max, h))
+Z = fc_model.predict(np.c_[xx.ravel(), yy.ravel()]) 
+
+
+#Z = Z.reshape(xx.shape)
+#plt.contourf(xx, yy, Z)
+#plt.axis('off')
