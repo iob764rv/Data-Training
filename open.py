42fe111592cc8a469be958c650bcd50b21e085bb
iob764rv <iic287m@gmail.com>
2020-07-10 00:08:03 -0400

    Update open.py

--- open.py
+++ open.py
@@ -275,7 +275,6 @@ class OpxRelPackage(object):
         if not version:
             return OpxRelPackage(name, None)
 
-
         match = re.match(r'\A([[(])([0-9][a-z0-9+-.:~]+)?,([0-9][a-z0-9+-.:~]+)?([])])\Z', version)
 
         if match:
