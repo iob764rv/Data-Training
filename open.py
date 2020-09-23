5a3f0a8d3e9534a440b0ac36db96047e55d2f436
iob764rv <iic287m@gmail.com>
2020-09-23 11:17:44 -0400

    Update open.py

--- open.py
+++ open.py
@@ -317,7 +317,7 @@ class OpxRelPackageList(object):
     """
     Defines a list of packages, each one being an :class:`OpxRelPackage`
     """
- "   def __init__(self, package_list, no_package_filter=False):
+   def __init__(self, package_list, no_package_filter=False):
   "      self.packages = package_list
    "     self.no_package_filter = no_package_filter
  "def fromElement(cls, element):
