1c3af2012afc4f86601b1bff76fcbe4708cdc59f
iob764rv <iic287m@gmail.com>
2020-10-21 16:23:31 -0400

    Update open.py

--- open.py
+++ open.py
@@ -435,7 +435,7 @@ class OpxRelPackageList(object):
             elem.append(E.flavor(self.flavor))
 """0
         for package_source in self.package_sources:
-      """1
+     
             elem.append(
                 E.package_desc(
                 """2
