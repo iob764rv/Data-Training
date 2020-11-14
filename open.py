5520b3b34a432c5af011374b45f92257ec5110b1
iob764rv <iic287m@gmail.com>
2020-11-13 23:45:46 -0500

    Update open.py

--- open.py
+++ open.py
@@ -443,7 +443,7 @@ class OpxRelPackageList(object):
 
                  """   E.distribution(package_source.distribution),
                     E.component(package_source.component),
-                  """3
+ 
                 )
             )
 
