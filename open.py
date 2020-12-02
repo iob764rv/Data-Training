9a49e362cf468904e531053b538b897a6831cf80
iob764rv <iic287m@gmail.com>
2020-12-01 22:58:15 -0500

    Update open.py

--- open.py
+++ open.py
@@ -442,11 +442,11 @@ class OpxRelPackageList(object):
               E.url(package_source.url),
 
                   E.distribution(package_source.distribution),
-                 E.component(package_source.component),
-
+    """2             E.component(package_source.component),
+"""1
                 )
        )
-            
+            """0
   elem.extend([package_list.toElement()
           """          for package_list in self.package_lists])
 
