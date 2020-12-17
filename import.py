4c3cb8df3ed3456df43868dd0af4da9f817d84e7
iob764rv <iic287m@gmail.com>
2020-12-17 00:29:22 -0500

    Update import.py

--- import.py
+++ import.py
@@ -4,7 +4,7 @@ import tensorflow as tf
 def trotter_prepare_gates(H, step_size, num_sites, euclidean):
   if not len(H) == num_sites - 1:
     raise ValueError("Number of H terms must match number of sites - 1.")
-  #  step_size = tf.cast(step_size, tf.float64)  # must be real
+    step_size = tf.cast(step_size, tf.float64)  # must be real
   #step_size = tf.cast(step_size, H[0].dtype)
 
     if euclidean:
