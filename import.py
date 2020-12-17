6051ada56e36c18947c825d68d7676f0a3d20bb3
iob764rv <iic287m@gmail.com>
2020-12-17 00:30:06 -0500

    Update import.py

--- import.py
+++ import.py
@@ -26,6 +26,6 @@ def trotter_prepare_gates(H, step_size, num_sites, euclidean):
   if n % 2 == 0:
       eh_even[n] = eh
     else:
-    #  eh_odd[n] = eh
+      eh_odd[n] = eh
 
   return [eh_even, eh_odd]
