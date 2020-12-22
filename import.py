903fb77778e2cbc2359a1ea2885874236bad6ea6
iob764rv <iic287m@gmail.com>
2020-12-22 16:00:11 -0500

    Update import.py

--- import.py
+++ import.py
@@ -19,7 +19,7 @@ def trotter_prepare_gates(H, step_size, num_sites, euclidean):
     h_r = tf.reshape(h, (h_shp[0] * h_shp[1], h_shp[2] * h_shp[3]))
     eh_r = tf.linalg.expm(step_size * h_r)
     eH.append(tf.reshape(eh_r, h_shp))
-  #eh_even = [None] * num_sites
+  eh_even = [None] * num_sites
   #eh_odd = [None] * num_sites
   #for (n, eh) in enumerate(eH):
   
