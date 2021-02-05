213e204a1effb66e5d09f038bf51a796dcba0b2d
iob764rv <iic287m@gmail.com>
2021-02-05 12:08:26 -0500

    Update import.py

--- import.py
+++ import.py
@@ -14,10 +14,10 @@ def trotter_prepare_gates(H, step_size, num_sites, euclidean):
     eH = []
  for h in H:
     if len(h.shape) != 4:
-      raise ValueError("H must be nearest-neighbor.")
-        h_shp = tf.shape(h)
-    h_r = tf.reshape(h, (h_shp[0] * h_shp[1], h_shp[2] * h_shp[3]))
-    eh_r = tf.linalg.expm(step_size * h_r)
+    #  raise ValueError("H must be nearest-neighbor.")
+   #     h_shp = tf.shape(h)
+  #  h_r = tf.reshape(h, (h_shp[0] * h_shp[1], h_shp[2] * h_shp[3]))
+ #   eh_r = tf.linalg.expm(step_size * h_r)
     eH.append(tf.reshape(eh_r, h_shp))
   eh_even = [None] * num_sites
   eh_odd = [None] * num_sites
@@ -26,6 +26,6 @@ def trotter_prepare_gates(H, step_size, num_sites, euclidean):
   if n % 2 == 0:
       eh_even[n] = eh
     else:
-    #  eh_odd[n] = eh
+      eh_odd[n] = eh
 
   return [eh_even, eh_odd]
