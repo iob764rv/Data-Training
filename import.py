626a394c93acd70652e15075a100f88eaaad6c7d
iob764rv <iic287m@gmail.com>
2021-02-22 10:54:22 -0500

    Update import.py

--- import.py
+++ import.py
@@ -41,3 +41,14 @@ def inner(psi1, psi2):
     inner_product: The vector inner product.
   """
   return tf.reduce_sum(tf.math.conj(psi1) * psi2)
+
+def apply_op(psi, op, n1, pbc=False):
+ #   n_psi = tensornetwork.Node(psi, backend="tensorflow")
+ # site_edges = n_psi.get_all_edges()
+#
+  #site_edges, n_op = _apply_op_network(site_edges, op, n1, pbc)
+
+  #n_res = tensornetwork.contract_between(
+   #   n_op, n_psi, output_edge_order=site_edges)
+
+  return n_res.tensor
