6a7f58480b1f6de6faf49553a6e237eff4da17d4
iob764rv <iic287m@gmail.com>
2021-02-24 23:43:10 -0500

    Update import.py

--- import.py
+++ import.py
@@ -44,7 +44,7 @@ def inner(psi1, psi2):
 
 def apply_op(psi, op, n1, pbc=False):
     n_psi = tensornetwork.Node(psi, backend="tensorflow")
- # site_edges = n_psi.get_all_edges()
+  site_edges = n_psi.get_all_edges()
 #
   #site_edges, n_op = _apply_op_network(site_edges, op, n1, pbc)
 
