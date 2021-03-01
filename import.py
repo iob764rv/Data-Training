2c24204ca8977e605cf38ea306a30df4fdcf1832
iob764rv <iic287m@gmail.com>
2021-03-01 08:48:46 -0500

    Update import.py

--- import.py
+++ import.py
@@ -46,7 +46,7 @@ def apply_op(psi, op, n1, pbc=False):
     n_psi = tensornetwork.Node(psi, backend="tensorflow")
   site_edges = n_psi.get_all_edges()
 #
-  #site_edges, n_op = _apply_op_network(site_edges, op, n1, pbc)
+  site_edges, n_op = _apply_op_network(site_edges, op, n1, pbc)
 
   #n_res = tensornetwork.contract_between(
    #   n_op, n_psi, output_edge_order=site_edges)
