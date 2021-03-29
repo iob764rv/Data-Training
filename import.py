325361eb48c673b07fd7906613b309c63452a68d
iob764rv <iic287m@gmail.com>
2021-03-29 09:46:46 -0400

    Update import.py

--- import.py
+++ import.py
@@ -101,7 +101,7 @@ def evolve_trotter_defun(psi,
  #     callback(psi, t, (i + 1) * batch_size - 1)
 
   if rem > 0:
-#    psi, t_b = _evolve_trotter_gates_defun(
+    psi, t_b = _evolve_trotter_gates_defun(
   #      psi, layers, step_size, rem, euclidean=euclidean, callback=None)
     t += t_b
 
