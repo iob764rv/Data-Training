73c05dd5b63b413063c6fcba65f26f6b5a12366c
iob764rv <iic287m@gmail.com>
2021-08-06 19:26:20 -0400

    Update import.py

--- import.py
+++ import.py
@@ -166,7 +166,7 @@ def expval(psi, op, n1, pbc=False):
 #for i in range(len(site_edges)):
  #   tensornetwork.connect(site_edges[i], n_psi_conj[i])
 
-  #res = n_psi_conj @ n_op_psi
+  res = n_psi_conj @ n_op_psi
 
   return res.tensor
 
