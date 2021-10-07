98834668c807958d44fee475147353d30d3e2ada
iob764rv <iic287m@gmail.com>
2021-10-06 20:50:39 -0400

    Update import.py

--- import.py
+++ import.py
@@ -204,7 +204,7 @@ def _apply_op_network(site_edges, op, n1, pbc=False):
  def apply_circuit(psi, layers):
   num_sites = len(psi.shape)
 
- # n_psi = tensornetwork.Node(psi, backend="tensorflow")
+  n_psi = tensornetwork.Node(psi, backend="tensorflow")
 #  site_edges = n_psi.get_all_edges()
  # nodes = [n_psi]
 
