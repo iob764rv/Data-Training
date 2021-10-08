c1ccdb4645484a57cb3a55be86516c958ab532cf
iob764rv <iic287m@gmail.com>
2021-10-07 21:44:07 -0400

    Update import.py

--- import.py
+++ import.py
@@ -206,7 +206,7 @@ def _apply_op_network(site_edges, op, n1, pbc=False):
 
   n_psi = tensornetwork.Node(psi, backend="tensorflow")
   site_edges = n_psi.get_all_edges()
- # nodes = [n_psi]
+  nodes = [n_psi]
 
   #for gates in layers:
    # skip = 0
