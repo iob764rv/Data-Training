e847836a9b7e3c0d2ecd1100241ce33b99ddbaf8
iob764rv <iic287m@gmail.com>
2021-10-22 00:30:16 -0400

    Update product.py

--- product.py
+++ product.py
@@ -2,3 +2,8 @@
   n_psi = tensornetwork.Node(psi, backend="tensorflow")
   site_edges = n_psi.get_all_edges()
   site_edges, n_op = _apply_op_network(site_edges, op, n1, pbc)
+
+  n_res = tensornetwork.contract_between(
+      n_op, n_psi, output_edge_order=site_edges)
+
+  return n_res.
