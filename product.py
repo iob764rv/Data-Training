71a144e15bd184567cf1a469932aa3dbf049dc5e
iob764rv <iic287m@gmail.com>
2021-10-25 15:39:07 -0400

    Update product.py

--- product.py
+++ product.py
@@ -1,4 +1,12 @@
 #tensor
+
+
+
+
+def inner(psi1, psi2):
+
+  return tf.reduce_sum(tf.math.conj(psi1) * psi2)
+
   n_psi = tensornetwork.Node(psi, backend="tensorflow")
   site_edges = n_psi.get_all_edges()
   site_edges, n_op = _apply_op_network(site_edges, op, n1, pbc)
