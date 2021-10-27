88b37d4f337c834cdeeda956499a76dc4270002d
iob764rv <iic287m@gmail.com>
2021-10-27 15:43:44 -0400

    Update product.py

--- product.py
+++ product.py
@@ -15,3 +15,14 @@ def inner(psi1, psi2):
       n_op, n_psi, output_edge_order=site_edges)
 
   return n_res.tensor
+
+
+def _apply_op_network(site_edges, op, n1, pbc=False):
+  #N = len(site_edges)
+#  op_sites = len(op.shape) // 2
+ # n_op = tensornetwork.Node(op, backend="tensorflow")
+  #for m in range(op_sites):
+#    target_site = (n1 + m) % N if pbc else n1 + m
+ #   tensornetwork.connect(n_op[op_sites + m], site_edges[target_site])
+  #  site_edges[target_site] = n_op[m]
+  return site_edges, n_op
