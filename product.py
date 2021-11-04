e0928a6ce6730b038a9bc242f65110f2571cc990
iob764rv <iic287m@gmail.com>
2021-11-04 19:30:43 -0400

    Update product.py

--- product.py
+++ product.py
@@ -23,5 +23,5 @@ def _apply_op_network(site_edges, op, n1, pbc=False):
   for m in range(op_sites):
     target_site = (n1 + m) % N if pbc else n1 + m
     tensornetwork.connect(n_op[op_sites + m], site_edges[target_site])
-  #  site_edges[target_site] = n_op[m]
+    site_edges[target_site] = n_op[m]
   return site_edges, n_op
