03f69ccb8d13fd2c954473343f4a4d5e1b6197e7
iob764rv <iic287m@gmail.com>
2021-11-09 22:55:25 -0500

    Update product.py

--- product.py
+++ product.py
@@ -35,7 +35,7 @@ def expval(psi, op, n1, pbc=False):
   n_psi_conj = tensornetwork.Node(tf.math.conj(psi), backend="tensorflow")
   
   #for i in range(len(site_edges)):
-   # tensornetwork.connect(site_edges[i], n_psi_conj[i])
+    tensornetwork.connect(site_edges[i], n_psi_conj[i])
     
   res = n_psi_conj @ n_op_psi
   
