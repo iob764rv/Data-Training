49ffc0fef0c0f51e33607fba03c4e313ca7b9513
iob764rv <iic287m@gmail.com>
2021-11-29 22:19:26 -0500

    Update product.py

--- product.py
+++ product.py
@@ -49,6 +49,6 @@ def evolve_trotter(psi,
                    callback=None):
 
   num_sites = len(psi.shape)
-  #layers = trotter_prepare_gates(H, step_size, num_sites, euclidean)
+  layers = trotter_prepare_gates(H, step_size, num_sites, euclidean)
   return _evolve_trotter_gates(
       psi, layers, step_size, num_steps, euclidean=euclidean, callback=callback)
