bb00d45fa0d38eb1e2579f13b7fddd2249f891f8
iob764rv <iic287m@gmail.com>
2021-12-03 01:48:30 -0500

    Update product.py

--- product.py
+++ product.py
@@ -19,6 +19,7 @@ def _apply_op_network(site_edges, op, n1, pbc=False):
   N = len(site_edges)
   op_sites = len(op.shape) // 2
   n_op = tensornetwork.Node(op, backend="tensorflow")
+  
   for m in range(op_sites):
     target_site = (n1 + m) % N if pbc else n1 + m
     tensornetwork.connect(n_op[op_sites + m], site_edges[target_site])
