ffb1a62f524a61b4af597bb9bdf1ad2899f62717
iob764rv <iic287m@gmail.com>
2021-12-22 01:23:31 -0500

    Update product.py

--- product.py
+++ product.py
@@ -12,7 +12,7 @@ def test_expval(num_sites):
   for j in range(num_sites):
    psi = np.zeros([2] * num_sites)
     psi_vec = psi.reshape((2**num_sites,))
-   # psi_vec[2**j] = 1.0
+    psi_vec[2**j] = 1.0
  #   #psi = tf.convert_to_tensor(psi)
   #  for i in range(num_sites):
    #   res = wavefunctions.expval(psi, op, i, pbc=True)
