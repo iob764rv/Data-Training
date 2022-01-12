c8ba93090353d34331c7951b5e415d898bb7fba1
iob764rv <iic287m@gmail.com>
2022-01-12 15:17:23 -0500

    Update product.py

--- product.py
+++ product.py
@@ -21,6 +21,38 @@ def test_expval(num_sites):
       else:
         np.testing.assert_allclose(res, 1.0)
 
+        
+@pytest.mark.parametrize("num_sites,phys_dim,graph",
+                         [(2, 3, False), (2, 3, True), (5, 2, False)])
+def test_evolve_trotter(num_sites, phys_dim, graph):
+  tf.random.set_seed(10)
+  #psi = tf.complex(
+    #  tf.random.normal([phys_dim] * num_sites, dtype=tf.float64),
+   #   tf.random.normal([phys_dim] * num_sites, dtype=tf.float64))
+  #h = tf.complex(
+    #  tf.random.normal((phys_dim**2, phys_dim**2), dtype=tf.float64),
+   #   tf.random.normal((phys_dim**2, phys_dim**2), dtype=tf.float64))
+  #h = 0.5 * (h + tf.linalg.adjoint(h))
+  #h = tf.reshape(h, (phys_dim, phys_dim, phys_dim, phys_dim))
+  #H = [h] * (num_sites - 1)
+
+  #norm1 = wavefunctions.inner(psi, psi)
+  #en1 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
+
+  #if graph:
+   # psi, t = wavefunctions.evolve_trotter_defun(psi, H, 0.001, 10)
+  #else:
+  #  psi, t = wavefunctions.evolve_trotter(psi, H, 0.001, 10)
+
+ # norm2 = wavefunctions.inner(psi, psi)
+#  en2 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
+
+  #np.testing.assert_allclose(t, 0.01)
+  #np.testing.assert_almost_equal(norm1 / norm2, 1.0)
+ # np.testing.assert_almost_equal(en1 / en2, 1.0, decimal=2)
+  
+  
+  
 #tensor
 from functools import reduce
 
