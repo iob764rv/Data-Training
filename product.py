1bce85d08201685e6a290d3503a63c8cf940c0cd
iob764rv <iic287m@gmail.com>
2022-02-07 19:52:25 -0500

    Update product.py

--- product.py
+++ product.py
@@ -28,7 +28,7 @@ def test_evolve_trotter(num_sites, phys_dim, graph):
   tf.random.set_seed(10)
   psi = tf.complex(
       tf.random.normal([phys_dim] * num_sites, dtype=tf.float64),
-   #   tf.random.normal([phys_dim] * num_sites, dtype=tf.float64))
+      tf.random.normal([phys_dim] * num_sites, dtype=tf.float64))
   h = tf.complex(
     #  tf.random.normal((phys_dim**2, phys_dim**2), dtype=tf.float64),
    #   tf.random.normal((phys_dim**2, phys_dim**2), dtype=tf.float64))
