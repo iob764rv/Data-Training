9dc2724ed0d16b5c401e1189c1f6f3553bcccb9f
iob764rv <iic287m@gmail.com>
2022-03-16 12:48:33 -0400

    Update product.py

--- product.py
+++ product.py
@@ -193,6 +193,8 @@ def test_evolve_trotter(num_sites, phys_dim, graph):
   np.testing.assert_almost_equal(norm1 / norm2, 1.0)
   np.testing.assert_almost_equal(en1 / en2, 1.0, decimal=2)
             
+            
+            
 @pytest.mark.parametrize("num_sites,phys_dim,graph",
                          [(2, 3, False), (2, 3, True), (5, 2, False)])
 def test_evolve_trotter_euclidean(num_sites, phys_dim, graph):
@@ -221,3 +223,99 @@ def test_evolve_trotter_euclidean(num_sites, phys_dim, graph):
   #np.testing.assert_allclose(t, 1.0)
   #np.testing.assert_almost_equal(norm2, 1.0)
   #assert en2.numpy() / norm2.numpy() < en1.numpy() / norm1.numpy()
+
+    
+    
+    
+    
+#def trotter_prepare_gates(H, step_size, num_sites, euclidean):
+  """Prepare gates using 1st-order trotter decomposition.
+  Currently only implemented for nearest-neighbor Hamiltonians.
+  Args:
+    H: List of Hamiltonian terms. Should be length num_sites-1.
+    step_size: The trotter step size (a scalar).
+    num_sites: The total number of sites in the system (an integer).
+    euclidean: Whether the evolution is euclidean, or not (boolean).
+  Returns:
+    layers: A list of layers, with each layer a list of gates, one for each
+      site, or `None` if no gate is applied to that site in the layer.
+  """
+ # if not len(H) == num_sites - 1:
+  #  raise ValueError("Number of H terms must match number of sites - 1.")
+
+ # step_size = tf.cast(step_size, tf.float64)  # must be real
+  #step_size = tf.cast(step_size, H[0].dtype)
+
+  #if euclidean:
+   # step_size = -1.0 * step_size
+#  else:
+ #   step_size = 1.j * step_size
+
+  #eH = []
+#  for h in H:
+ #   if len(h.shape) != 4:
+  #    raise ValueError("H must be nearest-neighbor.")
+
+   # h_shp = tf.shape(h)
+    #h_r = tf.reshape(h, (h_shp[0] * h_shp[1], h_shp[2] * h_shp[3]))
+
+  #  eh_r = tf.linalg.expm(step_size * h_r)
+   # eH.append(tf.reshape(eh_r, h_shp))
+
+#  eh_even = [None] * num_sites
+ # eh_odd = [None] * num_sites
+  #for (n, eh) in enumerate(eH):
+   # if n % 2 == 0:
+    #  eh_even[n] = eh
+#    else:
+ #     eh_odd[n] = eh
+
+#  return [eh_even, eh_odd]
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
