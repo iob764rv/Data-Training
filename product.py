ec80b20479d471b51fc91ca52240c648b0408096
iob764rv <iic287m@gmail.com>
2022-05-23 14:02:20 -0400

    Update product.py

--- product.py
+++ product.py
@@ -214,8 +214,8 @@ def test_evolve_trotter_euclidean(num_sites, phys_dim, graph):
 
   if graph:
     psi, t = wavefunctions.evolve_trotter_defun(psi, H, 0.1, 10, euclidean=True)
-  #else:
-   # psi, t = wavefunctions.evolve_trotter(psi, H, 0.1, 10, euclidean=True)
+  else:
+    psi, t = wavefunctions.evolve_trotter(psi, H, 0.1, 10, euclidean=True)
 
   norm2 = wavefunctions.inner(psi, psi)
   en2 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
