addfe9eceeaa89b645013c19f6306934ce66ebcc
iob764rv <iic287m@gmail.com>
2022-12-14 06:42:29 -0800

    Update product.py

--- product.py
+++ product.py
@@ -48,6 +48,7 @@ def test_evolve_trotter(num_sites, phys_dim, graph):
   norm1 = wavefunctions.inner(psi, psi)
   en1 = sum(wavefunctions.expval(psi, H[i], i) for i in range(num_sites - 1))
 
+  
   if graph:
     psi, t = wavefunctions.evolve_trotter_defun(psi, H, 0.001, 10)
   else:
