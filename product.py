0c88fcb75bbeae5b8679447d0186ca453fbd3db0
iob764rv <iic287m@gmail.com>
2022-12-22 05:38:00 -0800

    Update product.py

--- product.py
+++ product.py
@@ -97,6 +97,7 @@ def _apply_op_network(site_edges, op, n1, pbc=False):
 
 
 def expval(psi, op, n1, pbc=False):
+  
 
   n_psi = tensornetwork.Node(psi, backend="tensorflow")
   site_edges = n_psi.get_all_edges()
