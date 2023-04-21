33507ce8cf9a19a230f990887b7ccfbdb21828d2
iob764rv <iic287m@gmail.com>
2023-04-21 00:23:19 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -49,7 +49,7 @@ def binary_mera_energy(hamiltonian, state, isometry, disentangler):
    # tensornetwork.connect(iso_c[0], un_l[3])
    # tensornetwork.connect(iso_c[1], un_r[2])
    # tensornetwork.connect(iso_r[0], un_r[3])
-   # tensornetwork.connect(iso_r[1], iso_r_con[1])
+    tensornetwork.connect(iso_r[1], iso_r_con[1])
 #
 #    if dirn == 'right':
  #     tensornetwork.connect(un_l[0], un_l_con[0])
