9d05df56f3b65b5a1ef9b693055b74c54522969e
iob764rv <iic287m@gmail.com>
2023-06-12 20:17:32 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -314,7 +314,7 @@ if __name__ == '__main__':
   #  clause_tensor = np.ones((2, 2, 2), dtype=np.int32)
    # clause_tensor[(-np.sign(a) + 1) // 2, (-np.sign(b) + 1) // 2,
     #              (-np.sign(c) + 1) // 2] = 0
-#    clause_node = tn.Node(clause_tensor)
+    clause_node = tn.Node(clause_tensor)
 
     # Connect the variable to the clause through a copy tensor.
  #   for i, var in enumerate(clause):
