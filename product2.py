1cd536cb0f59477df5f624ce2424956fa6eb47c2
iob764rv <iic287m@gmail.com>
2023-06-20 22:41:03 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -303,7 +303,7 @@ if __name__ == '__main__':
   var_edges = []
 
   # Prepare the variable nodes.
-#  for _ in range(num_vars):
+  for _ in range(num_vars):
     new_node = tn.Node(np.ones(2, dtype=np.int32))
     var_nodes.append(new_node)
     var_edges.append(new_node[0])
