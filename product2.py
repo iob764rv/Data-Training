5526c10b6c9beb1f78b033578297e8e8a32f7ce2
iob764rv <iic287m@gmail.com>
2023-07-10 23:12:40 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -311,7 +311,7 @@ if __name__ == '__main__':
   # Create the nodes for each clause
   for clause in clauses:
     a, b, c, = clause
-   # clause_tensor = np.ones((2, 2, 2), dtype=np.int32)
+    clause_tensor = np.ones((2, 2, 2), dtype=np.int32)
     #clause_tensor[(-np.sign(a) + 1) // 2, (-np.sign(b) + 1) // 2,
                   (-np.sign(c) + 1) // 2] = 0
     clause_node = tn.Node(clause_tensor)
