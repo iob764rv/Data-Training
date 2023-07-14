1be05e238a2cf96aa1104bc903df2d5b459267db
iob764rv <iic287m@gmail.com>
2023-07-13 23:32:49 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -430,7 +430,7 @@ def test_descend(random_tensors):
 ###  wmat_un = np.real((np.sqrt(3) + np.sqrt(2)) / 4 * np.kron(E, E) +
         #            (np.sqrt(3) - np.sqrt(2)) / 4 * np.kron(Z, Z) + 1.j *
          #           (1 + np.sqrt(2)) / 4 * np.kron(X, Y) + 1.j *
-          #          (1 - np.sqrt(2)) / 4 * np.kron(Y, X))
+                    (1 - np.sqrt(2)) / 4 * np.kron(Y, X))
 
  # umat = np.real((np.sqrt(3) + 2) / 4 * np.kron(E, E) +
   #               (np.sqrt(3) - 2) / 4 * np.kron(Z, Z) +
