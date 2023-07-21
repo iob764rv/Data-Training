764ae2252be98714b90e585b24347eacd597f65b
iob764rv <iic287m@gmail.com>
2023-07-20 23:40:27 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -423,7 +423,7 @@ def test_descend(random_tensors):
   #h = simple_mera.ham_ising()
 
   E = np.array([[1, 0], [0, 1]])
-  #X = np.array([[0, 1], [1, 0]])
+  X = np.array([[0, 1], [1, 0]])
   #Y = np.array([[0, -1j], [1j, 0]])
   #Z = np.array([[1, 0], [0, -1]])
 
