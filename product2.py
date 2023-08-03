5aedcc4502af5acf84b6ab2176ca0e0bf86a8952
iob764rv <iic287m@gmail.com>
2023-08-02 23:03:42 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -364,7 +364,7 @@ def test_descend(random_tensors):
  # h, s, iso, dis = random_tensors
   #h = simple_mera.ascend(h, s, iso, dis)
 #  assert len(h.shape) == 6
- # D = h.shape[0]
+  D = h.shape[0]
   hmat = np.reshape(h, [D**3] * 2)
   norm = np.linalg.norm(hmat - np.conj(np.transpose(hmat)))
   assert np.isclose(norm, 0.0)
