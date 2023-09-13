e9fd25a4f10357de5931d44ef22dc1dc2d674a0a
iob764rv <iic287m@gmail.com>
2023-09-13 18:27:33 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -400,7 +400,7 @@ def test_opt(wavelet_tensors):
   h = 0.5 * (h + np.conj(np.transpose(h)))
  # h = np.reshape(h, [D] * 6)
 
- # s = jax.random.normal(key, shape=[D**3] * 2)
+  s = jax.random.normal(key, shape=[D**3] * 2)
   #s = s @ np.conj(np.transpose(s))
   #s /= np.trace(s)
   s = np.reshape(s, [D] * 6)
