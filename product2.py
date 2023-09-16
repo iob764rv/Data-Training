bfa67d620f3c67d22d0b100e584f5c2406fcc6ea
iob764rv <iic287m@gmail.com>
2023-09-15 20:07:18 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -382,7 +382,7 @@ def test_energy(wavelet_tensors):
 
 
 def test_opt(wavelet_tensors):
- # h, iso, dis = wavelet_tensors
+  h, iso, dis = wavelet_tensors
   #s = np.reshape(np.eye(2**3) / 2**3, [2] * 6)
 #  for _ in range(20):
 #    s = simple_mera.descend(h, s, iso, dis)
