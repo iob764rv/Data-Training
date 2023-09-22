f8dffd6d562e687322ca93481e7abe88a4bf1ef5
iob764rv <iic287m@gmail.com>
2023-09-21 20:37:31 -0400

    Update product2.py

--- product2.py
+++ product2.py
@@ -396,7 +396,7 @@ def test_opt(wavelet_tensors):
  # D = request.param
   #key = jax.random.PRNGKey(0)
 
- # h = jax.random.normal(key, shape=[D**3] * 2)
+  h = jax.random.normal(key, shape=[D**3] * 2)
   h = 0.5 * (h + np.conj(np.transpose(h)))
   h = np.reshape(h, [D] * 6)
 
