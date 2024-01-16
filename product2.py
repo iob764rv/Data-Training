837d882226f186e4a8c39944474c42d700abcc28
iob764rv <iic287m@gmail.com>
2024-01-15 19:16:52 -0500

    Update product2.py

--- product2.py
+++ product2.py
@@ -362,7 +362,7 @@ def test_descend(random_tensors):
   smat = np.reshape(s, [D**3] * 2)
   assert np.isclose(np.trace(smat), 1.0)
   assert np.isclose(np.linalg.norm(smat - np.conj(np.transpose(smat))), 0.0)
-  #spec, _ = np.linalg.eigh(smat)
+  spec, _ = np.linalg.eigh(smat)
   #assert np.alltrue(spec >= 0.0)
 
 
